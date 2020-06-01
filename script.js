35105c721a57ff1769c56092e3e5a6ec5f3a63fc
Glitch (hello-webpage) <none>
2020-06-01 19:09:58 +0000

    🐯✈️ Checkpoint ./script.js:16225047/122

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function displayWelcome(){
-  let username = document.querySelector("#username").innerHTML;
-  document.querySelector("div").style.display = "none;"
-  document.querySelector("#welcome").innerHTML = `Welcome, ${username}!`;
+  let username = document.querySelector("#username").value;
+  console.log(username);
+  alert("hello, "+username);
+  // document.querySelector("div").style.display = "none;"
+  // document.querySelector("#welcome").innerHTML = `Welcome, ${username}!`;
   
 }
